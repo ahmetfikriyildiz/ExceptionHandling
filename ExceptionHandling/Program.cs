c7433da52abeae9a34989a793d937e0889e5ac84
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-30 18:41:52 +0300

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Lütfen bir sayı giriniz: ");
+                int number = Convert.ToInt32(Console.ReadLine()); // Kullanıcıdan sayı girişi alınır
+
+                int square = number * number; // Sayının karesi hesaplanır
+                Console.WriteLine("Girdiğiniz sayının karesi: " + square);
+            }
+            catch (FormatException)
+            {
+                // Geçersiz giriş yapıldığında hata mesajı gösterilir
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExceptionHandling/README.md
@@ -0,0 +1,14 @@
+﻿TRY-CATCH
+
+Basit bir Exception Handling örneğidir. 
+
+Özellikler
+
+Geçerli Girişte Hesaplama: Kullanıcıdan alınan giriş geçerli bir sayı olduğunda, bu sayının karesi hesaplanır ve ekrana yazdırılır.
+Geçersiz Giriş Hatası: Kullanıcı geçersiz bir giriş yaparsa, kullanıcıya uyarı mesajı gösterilir.
+
+Kullanım
+
+Program başlatıldığında, kullanıcıdan bir sayı girmesi istenir.
+Eğer giriş geçerliyse, sayının karesi ekranda gösterilir.
+Giriş geçersizse (sayı dışında bir karakter girilirse), "Geçersiz giriş! Lütfen bir sayı giriniz." mesajı gösterilir.
